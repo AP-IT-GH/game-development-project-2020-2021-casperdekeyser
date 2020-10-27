efad14c84435170768e77905d3114a12f7a2d7fa
Casper De Keyser <casper.dekeyser@student.ap.be>
2020-10-27 18:29:05 +0100

    Added movement with keyboard

--- MakingAPlatformer/Hero.cs
+++ MakingAPlatformer/Hero.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,8 +9,14 @@ namespace MakingAPlatformer
 {
     public class Hero
     {
-        Texture2D heroTexture;
-        Animation animation;
+        public Vector2 Position { get; set; }
+        public Vector2 Direction;
+
+        private Texture2D heroTexture;
+        private Animation animation;
+        private int speed = 2;
+
+
         public Hero(Texture2D texture)
         {
             heroTexture = texture;
@@ -26,12 +33,25 @@ namespace MakingAPlatformer
 
         public void Update(GameTime gameTime)
         {
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Left))
+                Direction = new Vector2(-speed, 0);
+            
+            if (state.IsKeyDown(Keys.Right))
+                Direction = new Vector2(speed, 0);
+
+            Position += Direction;
             animation.Update(gameTime);
+        }
+
+        public void Move()
+        {
+
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(heroTexture, new Vector2(10, 10), animation.CurrentFrame.sourceRectangle, Color.White);
+            spriteBatch.Draw(heroTexture, Position, animation.CurrentFrame.sourceRectangle, Color.White);
 
         }
     }
