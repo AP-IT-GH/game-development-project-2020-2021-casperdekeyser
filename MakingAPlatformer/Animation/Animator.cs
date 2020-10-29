15cc0fccc40d51123f923193924b1c0e197ae4f7
Casper De Keyser <casper.dekeyser@student.ap.be>
2020-10-29 14:15:28 +0100

    Edited ignore file

--- MakingAPlatformer/Animation/Animator.cs
+++ MakingAPlatformer/Animation/Animator.cs
@@ -21,7 +21,7 @@ namespace MakingAPlatformer
             {
                 for (int i = 0; i <= animation.Width * (animation.FrameAmount - 1); i = i + animation.Width)
                 {
-                    animation.AddFrame(new AnimationFrame(new Rectangle(i, 0, 150, 150)));
+                    animation.AddFrame(new AnimationFrame(new Rectangle(i, 0, animation.Width, animation.Width)));
                 }
             }
         }
