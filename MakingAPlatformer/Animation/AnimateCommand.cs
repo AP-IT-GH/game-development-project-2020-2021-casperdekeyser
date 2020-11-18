42a733eac46654a2c84f550d65584430a1a6cd3a
Casper De Keyser <casper.dekeyser@student.ap.be>
2020-11-18 19:54:06 +0100

    Changed AnimateCommand

--- MakingAPlatformer/Animation/AnimateCommand.cs
+++ MakingAPlatformer/Animation/AnimateCommand.cs
@@ -9,24 +9,28 @@ namespace MakingAPlatformer
     {
         public void Execute(IAnimateable transform, Movement moveDirection)
         {
-            if (moveDirection == Movement.MoveRight)
-                transform.AnimToPlay = PossibleAnimations.RunLeft;
-            if (moveDirection == Movement.MoveLeft)
-                transform.AnimToPlay = PossibleAnimations.RunRight;
-            if (moveDirection == Movement.Idle)
-            {
-                if (transform.Animator.previousAnimation == PossibleAnimations.RunLeft)
-                    transform.AnimToPlay = PossibleAnimations.IdleLeft;
-                if (transform.Animator.previousAnimation == PossibleAnimations.RunRight)
-                    transform.AnimToPlay = PossibleAnimations.IdleRight;
-            }
-            if (moveDirection == Movement.Jump)
+            if (Hero.Jumping)
             {
                 if (transform.Animator.previousAnimation == PossibleAnimations.RunLeft || transform.Animator.previousAnimation == PossibleAnimations.IdleLeft)
                     transform.AnimToPlay = PossibleAnimations.JumpLeft;
                 if (transform.Animator.previousAnimation == PossibleAnimations.RunRight || transform.Animator.previousAnimation == PossibleAnimations.IdleRight)
                     transform.AnimToPlay = PossibleAnimations.JumpRight;
             }
+            else
+            {
+                if (moveDirection == Movement.MoveRight)
+                    transform.AnimToPlay = PossibleAnimations.RunLeft;
+                if (moveDirection == Movement.MoveLeft)
+                    transform.AnimToPlay = PossibleAnimations.RunRight;
+                if (moveDirection == Movement.Idle)
+                {
+                    if (transform.Animator.previousAnimation == PossibleAnimations.RunLeft || transform.Animator.previousAnimation == PossibleAnimations.JumpLeft)
+                        transform.AnimToPlay = PossibleAnimations.IdleLeft;
+                    if (transform.Animator.previousAnimation == PossibleAnimations.RunRight || transform.Animator.previousAnimation == PossibleAnimations.JumpRight)
+                        transform.AnimToPlay = PossibleAnimations.IdleRight;
+                }
+            }
+
         }
     }
 }
--- MakingAPlatformer/Animation/HeroAnimator.cs
+++ MakingAPlatformer/Animation/HeroAnimator.cs
@@ -10,40 +10,33 @@ namespace MakingAPlatformer
         public override Animation Animate(PossibleAnimations animToPlay)
         {
             previousAnimation = animToPlay;
-            if (Hero.Jumping)
+
+            if (animToPlay == PossibleAnimations.RunLeft)
+            {
+                return Animations[2];
+            }
+            if (animToPlay == PossibleAnimations.RunRight)
+            {
+                return Animations[3];
+            }
+            if (animToPlay == PossibleAnimations.IdleLeft)
+            {
+                 return Animations[0];
+            }
+            if (animToPlay == PossibleAnimations.IdleRight)
+            {
+                return Animations[1];
+            }
+            if (animToPlay == PossibleAnimations.JumpRight)
+            {
+                return Animations[5];
+            }
+            if (animToPlay == PossibleAnimations.JumpLeft)
             {
                 return Animations[4];
             }
-            else
-            {
-                if (animToPlay == PossibleAnimations.RunLeft)
-                {
-                    return Animations[2];
-                }
-                if (animToPlay == PossibleAnimations.RunRight)
-                {
-                    return Animations[3];
-                }
-                if (animToPlay == PossibleAnimations.IdleLeft)
-                {
-                    return Animations[0];
-                }
-                if (animToPlay == PossibleAnimations.IdleRight)
-                {
-                    return Animations[1];
-                }
-                if (animToPlay == PossibleAnimations.JumpRight)
-                {
-                    return Animations[5];
-                }
-                if (animToPlay == PossibleAnimations.JumpLeft)
-                {
-                    return Animations[4];
-                }
-                // default;
-                return Animations[0];
-
-            }
+            // default;
+            return Animations[0];
         }
     }
 }
